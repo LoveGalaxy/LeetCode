f552bc041ac211adc33e2cac9a380c5db8a8aa8d
guruL <695737315@qq.com>
2018-08-27 19:37:01 +0800

    537

---
+++ 537 Complex Number Multiplication/solution.py
@@ -0,0 +1,36 @@
+"""
+Given two strings representing two complex numbers.
+
+You need to return a string representing their multiplication. Note i2 = -1 according to the definition.
+
+Example 1:
+Input: "1+1i", "1+1i"
+Output: "0+2i"
+Explanation: (1 + i) * (1 + i) = 1 + i2 + 2 * i = 2i, and you need convert it to the form of 0+2i.
+Example 2:
+Input: "1+-1i", "1+-1i"
+Output: "0+-2i"
+Explanation: (1 - i) * (1 - i) = 1 + i2 - 2 * i = -2i, and you need convert it to the form of 0+-2i.
+Note:
+
+The input strings will not have extra blank.
+The input strings will be given in the form of a+bi, where the integer a and b will both belong to the range of [-100, 100]. And the output should be also in this form.
+"""
+class Solution(object):
+    def complexNumberMultiply(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        sa = a.split('+')
+        sb = b.split('+')
+        ar = int(sa[0])
+        ac = int(sa[1][:-1])
+        br = int(sb[0])
+        bc = int(sb[1][:-1])
+        
+        r = ar * br - ac * bc
+        c = ar * bc + ac * br
+        s = str(r) + '+' + str(c) + 'i'
+        return s
